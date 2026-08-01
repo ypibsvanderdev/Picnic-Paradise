80447b227697e4925e3e22b70c9aa54e014375ca
ypibsvanderdev <ypibsvanderdev@users.noreply.github.com>
2026-08-01 18:39:53 -0500

    Extend pickup time slots up to 8:00 PM in checkout dropdown and update event info time to 10:00 AM - 8:00 PM

--- js/cart.js
+++ js/cart.js
@@ -28,7 +28,9 @@ document.addEventListener('DOMContentLoaded', () => {
   const PICKUP_TIMES = [
     '10:00 AM', '10:30 AM', '11:00 AM', '11:30 AM',
     '12:00 PM', '12:30 PM', '1:00 PM', '1:30 PM',
-    '2:00 PM', '2:30 PM', '3:00 PM', '3:30 PM', '4:00 PM'
+    '2:00 PM', '2:30 PM', '3:00 PM', '3:30 PM',
+    '4:00 PM', '4:30 PM', '5:00 PM', '5:30 PM',
+    '6:00 PM', '6:30 PM', '7:00 PM', '7:30 PM', '8:00 PM'
   ];
 
   let currentDiscount = { code: null, rate: 0 };
